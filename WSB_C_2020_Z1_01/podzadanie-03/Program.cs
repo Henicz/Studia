c9578d0e894ba7311c5eeca1df943fd4328d8305
Michał Henicz <michalhenicz@gmail.com>
2020-05-01 16:26:21 +0200

    podzadanie-03-01 commit

--- WSB_C_2020_Z1_01/podzadanie-03/Program.cs
+++ WSB_C_2020_Z1_01/podzadanie-03/Program.cs
@@ -6,12 +6,37 @@ namespace podzadanie_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj pierwszą liczbę: ");
-            int first_number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj drugą liczbę: ");
-            int second_number = Convert.ToInt32(Console.ReadLine());
-            int score = first_number * second_number;
-            Console.WriteLine("Wynik " + first_number + "*" + second_number + " wynosi: " + score);
+            Console.WriteLine("Wybierz działanie:");
+            Console.WriteLine("1 - dodawanie");
+            Console.WriteLine("2 - odejmowanie");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            int first_number, second_number, score;
+
+            if (choose == 1)
+            {
+                Console.WriteLine("Podaj pierwszą liczbę: ");
+                first_number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj drugą liczbę: ");
+                second_number = Convert.ToInt32(Console.ReadLine());
+                score = first_number + second_number;
+                Console.WriteLine("Wynik " + first_number + " + " + second_number + " = " + score);
+            }
+            else if (choose == 2)
+            {
+                Console.WriteLine("Podaj pierwszą liczbę: ");
+                first_number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj drugą liczbę: ");
+                second_number = Convert.ToInt32(Console.ReadLine());
+                score = first_number - second_number;
+                Console.WriteLine("Wynik " + first_number + " - " + second_number + " = " + score);
+            }
+            else
+            {
+                Console.WriteLine("Nieobsługiwane");
+            }         
+           
+
+            
 
             Console.ReadLine();
         }
