f16df0f44cdf147500b7792824f90d3fe4084b73
Michał Henicz <michalhenicz@gmail.com>
2020-05-01 15:17:06 +0200

    podzadanie-02 commited

---
+++ podzadanie-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace podzadanie_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 12, 20 };
+
+            foreach(int number in numbers)
+            {
+                Int64 score = 1;
+                for(int x=1; x<(number+1); x++)
+                {
+                    score *= x;
+                }
+                Console.WriteLine("Wynik dla " + number + "! wynosi: " + score);
+            }
+            Console.ReadLine();
+        }
+    }
+}
