50042aca74864bc79cf82acb710e1deb346c7a39
Michał Henicz <michalhenicz@gmail.com>
2020-05-01 16:39:27 +0200

    commit podzadanie-03-03

--- WSB_C_2020_Z1_01/podzadanie-03/Program.cs
+++ WSB_C_2020_Z1_01/podzadanie-03/Program.cs
@@ -10,31 +10,45 @@ namespace podzadanie_03
             Console.WriteLine("1 - dodawanie");
             Console.WriteLine("2 - odejmowanie");
             Console.WriteLine("3 - mnożenie");
+            Console.WriteLine("4 - dzielenie");
             int variant = Convert.ToInt32(Console.ReadLine());
-            int first_number, second_number;
-
+            float first_number, second_number;
             switch (variant)
             {
                 case 1:
                     Console.Write("Podaj pierwszą liczbę: ");
-                    first_number = Convert.ToInt32(Console.ReadLine());
+                    first_number = Convert.ToSingle(Console.ReadLine());
                     Console.Write("Podaj drugą liczbę: ");
-                    second_number = Convert.ToInt32(Console.ReadLine());
+                    second_number = Convert.ToSingle(Console.ReadLine());
                     Console.WriteLine(first_number + " + " + second_number + " = " + (first_number+second_number));
                     break;
                 case 2:
                     Console.Write("Podaj pierwszą liczbę: ");
-                    first_number = Convert.ToInt32(Console.ReadLine());
+                    first_number = Convert.ToSingle(Console.ReadLine());
                     Console.Write("Podaj drugą liczbę: ");
-                    second_number = Convert.ToInt32(Console.ReadLine());
+                    second_number = Convert.ToSingle(Console.ReadLine());
                     Console.WriteLine(first_number + " - " + second_number + " = " + (first_number-second_number));
                     break;
                 case 3:
                     Console.Write("Podaj pierwszą liczbę: ");
-                    first_number = Convert.ToInt32(Console.ReadLine());
+                    first_number = Convert.ToSingle(Console.ReadLine());
                     Console.Write("Podaj drugą liczbę: ");
                     second_number = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine(first_number + " * " + second_number + " = " + (first_number*second_number));
+                    break;
+                case 4:
+                    Console.Write("Podaj pierwszą liczbę: ");
+                    first_number = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Podaj drugą liczbę: ");
+                    second_number = Convert.ToSingle(Console.ReadLine());
+                    if(second_number == 0)
+                    {
+                        Console.WriteLine("Nie można dzielić przez 0");
+                    }
+                    else
+                    {
+                        Console.WriteLine(first_number + " / " + second_number + " = " + (first_number / second_number));
+                    }
                     break;
                 default:
                     Console.WriteLine("Brak wybranej opcji");
